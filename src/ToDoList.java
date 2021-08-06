d4ff0c5e1a7b688b5d1ca51fc78d81c512e673ca
Corey McNeal <mcnealcorey@icloud.com>
2021-08-05 21:51:08 -0700

    Initial commit
    Barebones classes for the program

---
+++ src/ToDoList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoList {
+    private List<String> activityList;
+    public ToDoList() {
+        this.activityList = new ArrayList<>();
+    }
+}
---
+++ src/UserInterface.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    private Scanner reader;
+    
+    public UserInterface() {
+        this.reader = new Scanner(System.in);
+    }
+
+    public void start() {
+        
+    }
+}
