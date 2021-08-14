1d00b4394a3f86070cd7208fbc1be63286f54771
Corey McNeal <mcnealcorey@icloud.com>
2021-08-14 12:32:42 -0700

    Added build and show methods for each section of the UI. Mouse click events were fleshed out on line 137 as well.

--- src/Main/App.java
+++ src/Main/App.java
@@ -1,7 +1,6 @@
 package Main;
 
 import UserInterface.GUI;
-import UserInterface.UserInterface;
 
 public class App {
     public static void main(String[] args) throws Exception {
--- src/UserInterface/GUI.java
+++ src/UserInterface/GUI.java
@@ -1,7 +1,10 @@
 package UserInterface;
 
+import Main.ToDoList;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import javax.swing.BorderFactory;
@@ -9,22 +12,32 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 
 public class GUI implements ActionListener{
+    private Scanner reader = new Scanner(System.in);
+
     private JFrame frame;
     private JPanel panel;
-    private JButton addToListButton;
-    private JButton removeFromListButton;
-    private JButton editFromListButton;
-    private JLabel welcomeLabel;
+    private JButton addToListButton, removeFromListButton, editFromListButton;
+
+    private JTextField addEntry;
+    private JButton addButton, endAddingButton;
+    private JTextField removeEntry;
+    private JButton removeButton, endRemovingButton;
+    private JTextField editEntry;
+    private JButton editButton, endEditingButton;
+
+    private JLabel welcomeLabel, currentToDoList;
 
     public void start() {
-        buildInitialScreen();
+        buildScreens();
     }
 
-    public void buildInitialScreen() {
+    public void buildScreens() {
+        ToDoList tdList = new ToDoList(reader);
         frame = new JFrame();
         panel = new JPanel();
         
@@ -48,31 +61,92 @@ public class GUI implements ActionListener{
         panel.add(removeFromListButton);
 
         editFromListButton = new JButton("Edit from list");
+        editFromListButton.addActionListener(this);
         panel.add(editFromListButton);
 
+        currentToDoList = new JLabel("");
+        buildAddScreen();
+        buildRemoveScreen();
+        buildEditScreen();
 
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
 
-    private void addToListClicked() {
-
+    private void buildRemoveScreen() {
+        addEntry = new JTextField();
+        removeButton = new JButton("Remove");
+        endRemovingButton = new JButton("Exit");
+        addEntry.setVisible(false);
+        removeButton.setVisible(false);
+        endRemovingButton.setVisible(false);
+        panel.add(addEntry);
+        panel.add(removeButton);
+        panel.add(endRemovingButton);
     }
 
-    private void removeFromListClicked() {
-        
+    private void buildAddScreen() {
+        removeEntry = new JTextField();
+        addButton = new JButton("Add");
+        endAddingButton = new JButton("Exit");
+        removeEntry.setVisible(false);
+        addButton.setVisible(false);
+        endAddingButton.setVisible(false);
+        panel.add(removeEntry);
+        panel.add(addButton);
+        panel.add(endAddingButton);
     }
 
-    private void editFromListClicked() {
+    private void buildEditScreen() {
+        editEntry = new JTextField();
+        editButton = new JButton("Click to confirm entry to edit");
+        endEditingButton = new JButton("Exit");
+        editEntry.setVisible(false);
+        editButton.setVisible(false);
+        endEditingButton.setVisible(false);
+        panel.add(editEntry);
+        panel.add(editButton);
+        panel.add(endEditingButton);
+    }
 
+    private void showRemoveScreen() {
+        currentToDoList.setVisible(true);
+        removeEntry.setVisible(true);
+        removeButton.setVisible(true);
+        endRemovingButton.setVisible(true);
+        panel.validate();
+    }
+
+    public void showAddScreen() {
+        currentToDoList.setVisible(true);
+        addEntry.setVisible(true);
+        addButton.setVisible(true);
+        endAddingButton.setVisible(true);
+        panel.validate();
+    }
+
+    public void showEditScreen() {
+        currentToDoList.setVisible(true);
+        editEntry.setVisible(true);
+        editButton.setVisible(true);
+        endEditingButton.setVisible(true);
+        panel.validate();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == addToListButton) {
-            
+            System.out.println("1st");
+            hideWelcomeAndButtons();
+            showRemoveScreen();
         } else if (e.getSource() == removeFromListButton) {
-            
+            System.out.println("2nd");
+            hideWelcomeAndButtons();
+            showRemoveScreen();
+        } else if (e.getSource() == editFromListButton) {
+            System.out.println("3rd");
+            hideWelcomeAndButtons();
+            showEditScreen();
         }
         
     }
@@ -82,6 +156,7 @@ public class GUI implements ActionListener{
         removeFromListButton.setVisible(false);
         editFromListButton.setVisible(false);
         welcomeLabel.setVisible(false);
+        panel.revalidate();
     }
 
     private void showWelcomeAndButtons() {
@@ -89,5 +164,6 @@ public class GUI implements ActionListener{
         removeFromListButton.setVisible(true);
         editFromListButton.setVisible(true);
         welcomeLabel.setVisible(true);
+        panel.revalidate();
     }
 }
